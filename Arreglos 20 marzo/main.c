a1b9499d0d275907f68fae0ae7a4c05ea7555b38
sJosben <sergiochav1243@gmail.com>
2026-03-21 14:40:24 -0600

    Add files via upload

--- Arreglos 20 marzo/main.c
+++ Arreglos 20 marzo/main.c
@@ -4,15 +4,21 @@
 
 int main()
 {
-    int Num[5];
-    for(int i = 0; i < 5; i++){
-    scanf("%d", &Num[i]);
+    char Nombre[4][20], ArregloC[5];
 
+    printf("%d %d %d %d %d\n", &ArregloC[0], &ArregloC[1], &ArregloC[2], &ArregloC[3], &ArregloC[4]);
+    printf("%d\n", ArregloC);
+
+    for(int i; i<4; i++){
+    printf("\nCual es tu nombre completo?: ");
+    scanf("%s", Nombre[i]);
     }
 
-    for(int i = 0; i < 5; i++){
-    printf("Numero: %d Direccion de memoria: %d\n", Num[i], &Num[i]);
 
-    }
-return 0;
+    printf("\n\n--------------------------------\n");
+    printf("\n Tu nombre completo es: %s %s %s %s", Nombre[0], Nombre[1], Nombre[2], Nombre[3]);
+     printf("\n Direcciones de memoria: %d %d %d %d", &Nombre[0], &Nombre[1], &Nombre[2], &Nombre[3]);
+    printf("\n Direccion de memoria (Nombre): %d\n\n", &Nombre);
+
+    return 0;
 }
